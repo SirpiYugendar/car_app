ba8212613063a34713314d9020baf105048ed3e6
Sirpi Yugendar <yugendarsirpi12@gmail.com>
2021-01-13 14:08:08 +0530

    ListView into List tile

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(ListApper());
+  runApp(ListTileApper());
 }
 
 class App extends StatelessWidget {
@@ -211,61 +211,63 @@ class _FlexApperState extends State<FlexApper> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Flex',
-            style: GoogleFonts.pacifico().copyWith(fontSize: 30),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: rotateIcon,
-              tooltip: 'Direction',
-              onPressed: () {
-                setState(() {
-                  _vertical = !_vertical;
-                });
-              },
+          appBar: AppBar(
+            title: Text(
+              'Flex',
+              style: GoogleFonts.pacifico().copyWith(fontSize: 30),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(_vertical ? 'Vertical' : "Horizontal", style: GoogleFonts.pacifico(),),
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.aspect_ratio,
+            actions: <Widget>[
+              IconButton(
+                icon: rotateIcon,
+                tooltip: 'Direction',
+                onPressed: () {
+                  setState(() {
+                    _vertical = !_vertical;
+                  });
+                },
               ),
-              tooltip: 'Main Axis',
-              onPressed: () {
-                setState(() {
-                  _alignmentIndex++;
-                  if (_alignmentIndex >= _alignment.length) {
-                    _alignmentIndex = 0;
-                  }
-                });
-              },
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(_alignmentsText[_alignmentIndex], style: GoogleFonts.pacifico(),),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-            ),
-          ],
-        ),
-        body: Flex(
-          direction: _vertical ? Axis.vertical : Axis.horizontal,
-          mainAxisAlignment: _alignment[_alignmentIndex],
-          children: <Widget>[
-            redButton,
-            greenButton,
-            blueButton
-          ],
-        )
-      ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _vertical ? 'Vertical' : "Horizontal",
+                  style: GoogleFonts.pacifico(),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.aspect_ratio,
+                ),
+                tooltip: 'Main Axis',
+                onPressed: () {
+                  setState(() {
+                    _alignmentIndex++;
+                    if (_alignmentIndex >= _alignment.length) {
+                      _alignmentIndex = 0;
+                    }
+                  });
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _alignmentsText[_alignmentIndex],
+                  style: GoogleFonts.pacifico(),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+            ],
+          ),
+          body: Flex(
+            direction: _vertical ? Axis.vertical : Axis.horizontal,
+            mainAxisAlignment: _alignment[_alignmentIndex],
+            children: <Widget>[redButton, greenButton, blueButton],
+          )),
     );
   }
 }
+
 class ListApper extends StatelessWidget {
   const ListApper({Key key}) : super(key: key);
 
@@ -273,31 +275,190 @@ class ListApper extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-          home: Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('ListView', style: GoogleFonts.pacifico().copyWith(fontSize: 30),)),
+          title: Center(
+              child: Text(
+            'ListView',
+            style: GoogleFonts.pacifico().copyWith(fontSize: 30),
+          )),
         ),
-            body: Container(
+        body: Container(
           margin: EdgeInsets.symmetric(vertical: 20),
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: <Widget>[
-              Container(width: 150, color: Colors.redAccent,),
-              Container(width: 150, color: Colors.greenAccent,),
-              Container(width: 150, color: Colors.blueAccent,),
-              Container(width: 150, color: Colors.yellowAccent,),
-              Container(width: 150, color: Colors.purpleAccent,),
-              Container(width: 150, color: Colors.pinkAccent,),
-              Container(width: 150, color: Colors.black,),
-              Container(width: 150, color: Colors.redAccent,),
-              Container(width: 150, color: Colors.greenAccent,),
-              Container(width: 150, color: Colors.blueAccent,),
-              Container(width: 150, color: Colors.yellowAccent,),
-              Container(width: 150, color: Colors.purpleAccent,),
-              Container(width: 150, color: Colors.pinkAccent,),
-              Container(width: 150, color: Colors.black,),
+              Container(
+                width: 150,
+                color: Colors.redAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.greenAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.blueAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.yellowAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.purpleAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.pinkAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.black,
+              ),
+              Container(
+                width: 150,
+                color: Colors.redAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.greenAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.blueAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.yellowAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.purpleAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.pinkAccent,
+              ),
+              Container(
+                width: 150,
+                color: Colors.black,
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ListTileApper extends StatefulWidget {
+  ListTileApper({Key key}) : super(key: key);
+
+  @override
+  _ListTileApperState createState() => _ListTileApperState();
+}
+
+class _ListTileApperState extends State<ListTileApper> {
+  int _selectedIndex = 0;
+  static const TEXT_STYLE_NORMAL = TextStyle(
+      color: Colors.black, fontSize: 18, fontWeight: FontWeight.normal);
+  static const TEXT_STYLE_SELECTED =
+      TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold);
+  final TextFormField _fontsTextField = TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(Icons.format_size),
+        hintText: 'FontSize',
+        labelText: 'Enter the Font Size'),
+  );
+  final TextFormField _historyTextField = TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(Icons.history),
+        hintText: 'number of days',
+        labelText: 'Enter Number of days'),
+  );
+  final TextFormField _languageTextField = TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(Icons.language),
+        hintText: 'Tamil or English',
+        labelText: 'Enter your Language'),
+  );
+  select(index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ListTile accesibilityListTile = ListTile(
+      leading: Icon(Icons.accessibility),
+      title: Text(
+        'Accessibility',
+        style: _selectedIndex == 0 ? TEXT_STYLE_SELECTED : TEXT_STYLE_NORMAL,
+      ),
+      subtitle: Text('Accesibility Settings'),
+      trailing: Icon(Icons.settings),
+      onTap: () => select(0),
+    );
+    final ListTile historyListTile = ListTile(
+      leading: Icon(Icons.accessibility),
+      title: Text(
+        'History',
+        style: _selectedIndex == 1 ? TEXT_STYLE_SELECTED : TEXT_STYLE_NORMAL,
+      ),
+      subtitle: Text('History Settings'),
+      trailing: Icon(Icons.settings),
+      onTap: () => select(1),
+    );
+    final ListTile languageListTile = ListTile(
+      leading: Icon(Icons.accessibility),
+      title: Text(
+        'Language',
+        style: _selectedIndex == 2 ? TEXT_STYLE_SELECTED : TEXT_STYLE_NORMAL,
+      ),
+      subtitle: Text('Language Settings'),
+      trailing: Icon(Icons.settings),
+      onTap: () => select(2),
+    );
+    final String selectionTitle = (_selectedIndex == 0
+            ? 'Accestibility'
+            : _selectedIndex == 1
+                ? 'History'
+                : 'Language') +
+        'Settings';
+    final TextFormField selectionFormField = _selectedIndex == 0
+        ? _fontsTextField
+        : _selectedIndex == 1
+            ? _historyTextField
+            : _languageTextField;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text(
+          'ListTile',
+          style: GoogleFonts.pacifico().copyWith(fontSize: 30),
+        )),
+        body: ListView(
+          children: <Widget>[
+            accesibilityListTile,
+            historyListTile,
+            languageListTile
+          ],
+        ),
+        bottomSheet: Container(
+          color: Color(0xFFCFC8F5),
+          padding: EdgeInsets.all(10),
+          child: Container(
+              constraints: BoxConstraints(maxHeight: 200),
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.settings),
+                  Text(selectionTitle),
+                  Expanded(child: selectionFormField)
+                ],
+              )),
         ),
       ),
     );
